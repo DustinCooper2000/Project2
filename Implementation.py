3193c1e90c1f4852a83a4a7ac591fb05d540d92b
cooperd <cooperd@berea.edu>
2020-02-27 20:12:17 -0500

    Created the basics of a scraper

--- Implementation.py
+++ Implementation.py
@@ -1,1 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas
+import matplotlib
+import lxml
+
+def scraper():
+    url = "https://en.wikisource.org/wiki/Portal:State_of_the_Union_Speeches_by_United_States_Presidents"
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, "lxml")
+    print(soup)
+
+def main():
+    scraper()
+
+
+main()
 
